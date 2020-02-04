63abac0d720925257be02321e6b71a5e3581eda0
raghavm8 <raghav.mittal1109@gmail.com>
2020-02-04 18:06:06 +0530

    front end added

---
+++ todo/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import createTodo from "./components/createTodo";
+import editTodo from "./components/editTodo";
+import todoList from "./components/todoList";
+import logo from "./logo.png";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+              <img src={logo} width="30" height="30" alt="CodingTheSmartWay.com" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+            <div className="collpase nav-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todo</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <Route path="/" exact component={todoList} />
+          <Route path="/edit/:id" component={editTodo} />
+          <Route path="/create" component={createTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
